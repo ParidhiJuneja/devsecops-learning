16feec1bd87255ad4ff144250d480c34472c5111
Paridhi <junejaparidhi@gmail.com>
2026-08-17 14:03:39 +0530

    Add GitGuardian secret scanning test

--- app/app.py
+++ app/app.py
@@ -9,3 +9,6 @@ def subtract(a, b):
 if __name__ == "__main__":
     print("V2 - 2 + 3 =", add(2, 3))
     print("V2 - 5 - 2 =", subtract(5, 2))
+
+# Dummy secret for GitGuardian testing
+TEST_API_KEY = "sk_test_FAKE1234567890SECRET"
