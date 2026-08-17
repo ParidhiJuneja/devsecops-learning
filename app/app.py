12087826e2472b67e660d2e558513dc913e301ec
Paridhi <junejaparidhi@gmail.com>
2026-08-17 15:09:01 +0530

    Remove GitGuardian test secret

--- app/app.py
+++ app/app.py
@@ -10,5 +10,4 @@ if __name__ == "__main__":
     print("V2 - 2 + 3 =", add(2, 3))
     print("V2 - 5 - 2 =", subtract(5, 2))
 
-# Dummy secret for GitGuardian testing
-TEST_API_KEY = "sk_test_FAKE1234567890SECRET"
+
